8008b47aa7090d54ac26d5140c073924f3d20057
vaibhav kukreja <kukreja.vaibhav@gmail.com>
2018-05-08 12:31:12 +0200

    My identity for @lewagon in the gitconfig

--- install.sh
+++ install.sh
@@ -33,7 +33,7 @@ ZSH_PLUGINS_DIR="$HOME/.oh-my-zsh/custom/plugins"
 mkdir -p "$ZSH_PLUGINS_DIR" && cd "$ZSH_PLUGINS_DIR"
 if [ ! -d "$ZSH_PLUGINS_DIR/zsh-syntax-highlighting" ]; then
   echo "-----> Installing zsh plugin 'zsh-syntax-highlighting'..."
-  git clone git://github.com/zsh-users/zsh-syntax-highlighting.git
+  git clone --progress --verbose git://github.com/zsh-users/zsh-syntax-highlighting.git
 fi
 cd "$CURRENT_DIR"
 
@@ -61,4 +61,4 @@ ln -s $PWD/Package\ Control.sublime-settings $SUBL_PATH/Packages/User/Package\ C
 
 zsh ~/.zshrc
 
-echo "👌  Carry on with git setup!"
+echo "👌  Carry on with git setu!"
